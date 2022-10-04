037b47142dcac20f71bd4f6d894a36d130872bf0
shah <liyaktshaikh07@gmail.com>
2022-10-05 00:07:19 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, {useState} from 'react'
 import FileUploader from './components/FileUploader';
+import * as XLSX from 'xlsx';
 
 const App = () => {
   const [selectedfile, setSelectedfile] = useState(null);
-  const submitForm = () => {
-    console.log("FILE IS::", selectedfile);
+  const submitForm = (e) => {
+    e.preventDefault();
+    const reader = new FileReader();
+
+    reader.onload = (evt) => {
+      const bstr = evt.target.result;
+      const wb = XLSX.read(bstr, {type: "binary"});
+      const wsname = wb.SheetNames[0];
+      const ws =  wb.Sheets[wsname];
+      const data = XLSX.utils.sheet_to_csv(ws, {header: 1});
+      console.log(data);
+    };
+    reader.readAsBinaryString(selectedfile);
   };
 
   return (
     <div className='App'>
+      <div>
       <h4>File :</h4><span>{ selectedfile && selectedfile.name}</span>
+      </div>
       <form>
         <FileUploader
           onFileSelectSuccess={(file) => setSelectedfile(file)}
--- src/components/FileUploader.jsx
+++ src/components/FileUploader.jsx
@@ -2,22 +2,31 @@ import React, {useRef} from 'react'
 
 const FileUploader = ({onFileSelectError, onFileSelectSuccess}) => {
     const fileInput = useRef(null);
+    const onReset = () => {
+      fileInput.current.value = null;
+    };
 
     const handleFileInput = (e) => {
         //handle validations
         const file = e.target.files[0];
         console.log(file);
-        if(file.type === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-            onFileSelectSuccess(file)
+        if(!file){
+          return;
+        }
+        if(file.type === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" || file.type === "text/csv" || file.type === "application/vnd.ms-excel")
+            {
+              onFileSelectSuccess(file)
+            }
         else
-            onFileSelectError({error: "Please choose .xlsx file only"});
-           
+        {
+            onFileSelectError({error: "Please upload valid Excel file"});
+            onReset();
+        }  
     }
 
   return (
     <div className="file-uploader">
-        <input type="file" onChange={handleFileInput} />
-        <button onClick={e => fileInput.current && fileInput.current.click()} className="btn btn-primary"></button>
+        <input ref={fileInput} type="file" onChange={handleFileInput} />
     </div>
   )
 }
